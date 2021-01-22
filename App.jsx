0a6bc51aa8cf86f5707fdef52c2c98ae1ac14c74
JurcaRobert <54647983+JurcaRobert@users.noreply.github.com>
2021-01-22 13:47:53 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
+
+
+
+
+
+function App() {
+  return (
+   <Router> 
+        
+          < GlobalStyle />
+           < Hero />
+           < Products heading='Chose your favorite' data ={productData} />
+           < Feature />
+           < Products heading='Chose your favorite' data ={productDataTwo} />
+           < Products heading='Chose your favorite' data ={productData} />
+           < Footer />
+     </Router>
+      
+    
+  );
+}
+
+export default App;
